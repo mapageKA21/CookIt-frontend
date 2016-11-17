58c299b060b7e9245a99684ed10c553b3ce38d53
Ian Salt <ian@isalt.digital>
2016-11-17 14:59:46 +0100

    feat: bootstrap css added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  // works like a CSS selector
+  template: ``,
+  styles: [`
+  
+  `]
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = '';
 }
