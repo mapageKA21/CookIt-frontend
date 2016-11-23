57492a2f8b3e9aea39feeea57e7233fe290638b8
Ruben Sanz <info@rubensanz.es>
2016-11-23 10:43:18 +0100

    feat: On keypress on search bar updates the recipe list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,20 +3,24 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service'
 import { LoginComponent } from '../login/login.component';
 import { AuthEvent } from '../auth-event';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  providers: [AuthService]
+  providers: [AuthService, ApiService]
 })
 export class HeaderComponent implements OnInit {
 
   username: string = '';
 
-  constructor(private authClient: AuthService, private authEvList: AuthEvent) {
+  constructor(
+    private authClient: AuthService,
+    private authEvList: AuthEvent,
+    private apiClient: ApiService
+  ) {
     this.authEvList.subscribe({
       next: data => {
-        console.log(data);
         this.username = data.username;
       }
     });
@@ -28,6 +32,14 @@ export class HeaderComponent implements OnInit {
 
   isLogged() {
     return true;
+  }
+
+  search(terms: string) {
+    if (terms.length>2) {
+      this.apiClient.getRecipes(terms);
+    } else {
+
+    }
   }
 
 }
